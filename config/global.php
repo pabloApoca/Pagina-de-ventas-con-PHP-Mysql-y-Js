47f1a91cb80bfd338918913ed00238eea8b47b4b
Pablo Lopez <pablo_mtv@hotmail.com>
2020-03-10 15:53:14 -0300

    Añadi todo lo del sistema hasta categoria

---
+++ config/global.php
@@ -0,0 +1,20 @@
+<?php 
+//ip de pc servidor de base de datos
+define("BD_HOST","localhost");
+
+//Nombre de la base de datos
+define("DB_NAME","dbsistema");
+
+//Usuario de la base de datos
+define("DB_USERNAME", "root");
+
+//Contraseña del usuario de la base datos
+define("DB_PASSWORD", "");
+
+//Definimos la codificacion de los caracteres
+define("DB_ENCODE","utf8");
+
+//Definimos una constante caomo nombre del proyecto
+define("PRO_NOMBRE", "ITVentas");
+
+?>
